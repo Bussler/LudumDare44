6edc44826bdaf5793b285e9c78d2787a95fa53d9
Joshua Weggartner <Joshua.Weggartner@gmx.de>
2019-04-27 12:48:04 +0200

    enemy behaviour ausbau

---
+++ LudumDare44/Assets/Assets/Scripts/Enemy/DamagePlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlane : MonoBehaviour {
+
+    public float growTime;
+    public float lifeTime;
+    
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Grow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    IEnumerator Grow()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 newScale = new Vector3(transform.localScale.x + 0.01f, transform.localScale.y + 0.01f, transform.localScale.z + 0.01f);
+            transform.localScale = newScale;
+            yield return new WaitForSeconds(growTime / 100);
+
+        }
+    }
+}
