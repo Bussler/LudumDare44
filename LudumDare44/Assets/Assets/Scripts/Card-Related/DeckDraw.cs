17b7cf64e7317436c0b258295e5300f118ff52fe
Bussler <maarten.bussler@gmail.com>
2019-04-27 12:10:19 +0200

    Further card logik, effects still missing

---
+++ LudumDare44/Assets/Assets/Scripts/Card-Related/DeckDraw.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DeckDraw : MonoBehaviour, IPointerClickHandler
+{
+
+    public Transform playerhand;
+
+    public GameObject cardPrefab;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Haha");
+        Card curCard = GameObject.Find("DeckManager").GetComponent<DeckManager>().drawCard(0);
+        spawnCard(curCard, playerhand);
+
+
+    }
+
+    public void spawnCard(Card c, Transform hand)
+    {
+        GameObject card = Instantiate(cardPrefab) as GameObject;
+        card.GetComponent<DisplayCard>().card = c;
+        card.GetComponent<DisplayCard>().applyInfo();
+
+        card.transform.SetParent(playerhand);
+
+    }
+
+}
---
+++ LudumDare44/Assets/Assets/Scripts/Card-Related/DeckManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour {
+
+    public List<Card> PlayerDeck;
+
+    private List<Card> OriginalDeck;
+
+    public static List<Card> Graveyard;
+
+    private void Start()
+    {
+        OriginalDeck = new List<Card>(PlayerDeck);
+        shuffle(PlayerDeck);
+    }
+
+    public Card drawCard(int i)
+    {
+        Card curCard = PlayerDeck[i];
+        PlayerDeck.RemoveAt(i);
+        if (PlayerDeck.Count <= 0)
+        {
+            resetDeck();
+
+        }
+        return curCard;
+    }
+
+    public static void addGraveyard(Card card)
+    {
+        Graveyard.Add(card);
+
+    }
+
+    public void shuffle<T>(List<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+
+    }
+
+    public void refillDeck()
+    {
+        PlayerDeck = new List<Card>(Graveyard);
+        shuffle(PlayerDeck);//shuffle
+        Graveyard.Clear();
+    }
+
+    public void resetDeck()
+    {
+        PlayerDeck = new List<Card>(OriginalDeck);
+        shuffle(PlayerDeck);//shuffle
+    }
+
+}
