b3230ae36f0a8ecce07bdf38e570bffb26a9f9b0
Bussler <maarten.bussler@gmail.com>
2019-04-27 19:08:20 +0200

    updated effects

--- LudumDare44/Assets/Assets/Scripts/Card-Related/Cardeffects/ApplyDmgOverTime.cs
+++ LudumDare44/Assets/Assets/Scripts/Card-Related/Cardeffects/ApplyDmgOverTime.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class ApplyDmgOverTime : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    public int time = 5;//time to live
+    public int dmg = 1; //dmg to apply each second
+
+    public bool doesDmg = true;
+
+    private float elapsedTime=0f;
+
+    public float timeInterval = 1;
+
+    private float ElapsedInterval;
+
 	void Update () {
-		
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime>=time)
+        {
+            Destroy(this.gameObject);
+        }
+
 	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ElapsedInterval += Time.deltaTime;
+        if (ElapsedInterval >= timeInterval)
+        {
+            if (!doesDmg && collision.gameObject.name=="Player")
+            {
+                PlayerManager plM = GameObject.FindObjectOfType<PlayerManager>();
+                if (plM != null)
+                {
+                    plM.GetHealth(dmg);
+                    Debug.Log("Player healed");
+                }
+            }
+            if (doesDmg)
+            {
+                Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.TakeDmage(-dmg);
+                }
+            }
+
+            ElapsedInterval = 0f;
+        }
+    }
+
 }
