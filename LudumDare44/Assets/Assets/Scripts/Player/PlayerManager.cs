9afd208ec8b47719c564820ce0515a22f7e74a9f
Luca <hohmann.luca@tutanota.com>
2019-04-27 14:29:10 +0200

    Added joshis things back

--- LudumDare44/Assets/Assets/Scripts/Player/PlayerManager.cs
+++ LudumDare44/Assets/Assets/Scripts/Player/PlayerManager.cs
@@ -7,7 +7,7 @@ public class PlayerManager : MonoBehaviour{
 
 	[SerializeField]
 	private int playerHealth, playerDamage;
-	
+	private int playerCurrentHealth;
 	[SerializeField]
 	private float effectTimer;
 
@@ -21,8 +21,13 @@ public class PlayerManager : MonoBehaviour{
 	[SerializeField] 
 	private GameObject sprites;
 	
+	
+	
 	// Use this for initialization
 	void Start () {
+	
+		playerCurrentHealth = playerHealth;
+		
 		playerAbilities = new BaseEffect[1];
 		
 		playerAbilities[0] = new AoESampleAbility(Instantiate(sprites, transform.position, sprites.transform.rotation), mainCamera);
@@ -33,8 +38,25 @@ public class PlayerManager : MonoBehaviour{
 	// Update is called once per frame
 	void Update () {
 		playerAbilities[0].displayEffect();
+		if (playerCurrentHealth <= 0)
+		{
+			Die();
+		}
 	}
 	
-	
+	public void TakeDamage(int amount)
+	{
+		playerCurrentHealth -= amount;
+	}
+
+	public void GetHealth(int amount)
+	{
+
+	}
+
+	public void Die()
+	{
+		//GameOver
+	}
 	
 }
