eda041910ec354f50e7371e22a5c593c051712d8
Luca <hohmann.luca@tutanota.com>
2019-04-27 13:18:53 +0200

    Base effect, PlayerManager
    Added:
    1) Base effect - abstract class which will be extended by every ability
    2) Player Manager - setup of player manager

---
+++ LudumDare44/Assets/Assets/Scripts/Player Abilities/BaseEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEffect : MonoBehaviour
+{
+	//animator for possible animations
+	[SerializeField]
+	private Animator animator;
+
+	//sprite which displays how the ability works (e.g. for AoE the place it lands)
+	[SerializeField] 
+	private Sprite effectDisplaySprite;
+	
+	public abstract void displayEffect();
+	public abstract void playEffect();
+
+	
+}
---
+++ LudumDare44/Assets/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour{
+
+	[SerializeField]
+	private int playerHealth, playerDamage;
+	[SerializeField]
+	private float effectTimer;
+
+	private Boolean canUseAbilities;
+	
+	[SerializeField]
+	private BaseEffect[] gameEffects;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	
+}
