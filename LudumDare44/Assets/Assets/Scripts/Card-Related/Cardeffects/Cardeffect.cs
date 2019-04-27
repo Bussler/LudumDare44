e3dbf5c866b18fc546cefdb47bb64ed7196164e8
Bussler <maarten.bussler@gmail.com>
2019-04-27 13:01:18 +0200

    Effect

---
+++ LudumDare44/Assets/Assets/Scripts/Card-Related/Cardeffects/Cardeffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cardeffect :MonoBehaviour{
+
+    public Cardeffect()
+    {
+
+    }
+
+    public virtual void Apply()
+    {
+
+    }
+	
+}
---
+++ LudumDare44/Assets/Assets/Scripts/Card-Related/Cardeffects/TestEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEffect : Cardeffect {
+
+
+    public override void Apply()
+    {
+        Debug.Log("Success Effect activation");
+    }
+
+}
