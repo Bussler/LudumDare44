ab0c35b872bae3e0641552fbb4316b424b69bc57
Bussler <maarten.bussler@gmail.com>
2019-04-27 19:29:44 +0200

    FirreBall effect

---
+++ LudumDare44/Assets/Assets/Scripts/Card-Related/Cardeffects/FireBallMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallMovement : MonoBehaviour {
+
+    public float speed = 10;
+
+    public float dmg = 5;
+
+    public Vector3 Target;
+
+    private bool ready;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ready)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, Target, speed*Time.deltaTime);
+        }
+
+	}
+
+    public void UpdateValues( Vector3 t)
+    {
+        Target = t;
+        ready = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDmage(-dmg);
+        }
+    }
+
+}
