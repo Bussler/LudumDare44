05cce199b3c3d116e6199cf07f381822b4a171c3
Bussler <maarten.bussler@gmail.com>
2019-04-28 16:59:23 +0200

    added menu scene

---
+++ LudumDare44/Assets/Assets/Scripts/MenuMenager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuMenager : MonoBehaviour {
+
+    public GameObject helpText;
+
+    public void activateText()
+    {
+        if (helpText.active)
+        {
+            helpText.SetActive(false);
+        }
+        else
+        {
+            helpText.SetActive(true);
+        }
+
+    }
+
+    public void loadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+}
