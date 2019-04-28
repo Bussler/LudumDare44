a1892e9d6c0d31a996936042b2907f6e8c5d4bf8
Bussler <maarten.bussler@gmail.com>
2019-04-28 10:37:06 +0200

    Implemented new card effects

--- LudumDare44/Assets/Assets/Scripts/Player Abilities/BaseEffect.cs
+++ LudumDare44/Assets/Assets/Scripts/Player Abilities/BaseEffect.cs
@@ -2,7 +2,7 @@
  using System.Collections.Generic;
    using UnityEngine;
        
-       public abstract class BaseEffect{
+       public abstract class BaseEffect: MonoBehaviour{
        	//animator for possible animations
        	[SerializeField]
        	protected Animator animator;
---
+++ LudumDare44/Assets/Assets/Scripts/Player Abilities/CreateMultipleFireBalls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMultipleFireBalls : MonoBehaviour {
+
+    public GameObject FireBall;
+    Transform playerPos;
+    Vector3 enemyPos;
+
+    int amtOfBalls;
+
+    public void UpdateValues(Vector3 enemy, Transform player, int amt)
+    {
+        playerPos = player;
+        enemyPos = enemy;
+        amtOfBalls = amt;
+
+        StartCoroutine(spawnFBalls());
+
+    }
+
+    IEnumerator spawnFBalls()
+    {
+        for (int i=0;i<amtOfBalls;i++)
+        {
+            GameObject myBall = GameObject.Instantiate(FireBall, playerPos.position, Quaternion.identity);
+            myBall.GetComponent<FireBallMovement>().UpdateValues(enemyPos);
+            yield return new WaitForSeconds(0.3f);
+        }
+        yield return null;
+    }
+
+}
