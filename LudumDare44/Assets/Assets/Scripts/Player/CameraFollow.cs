b3262e35baa4f428bb1cc4818c0ab2375bf3e03e
Luca <hohmann.luca@tutanota.com>
2019-04-28 12:15:59 +0200

    camera follow, particles

---
+++ LudumDare44/Assets/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject player;
+	
+	
+	// Update is called once per frame
+	void LateUpdate (){
+		transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+	}
+}
